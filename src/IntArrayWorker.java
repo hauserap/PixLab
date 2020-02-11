8424906a1f747a7216b0bd66b9f6de66f3996fe2
Alex Pope Hauser <hauserap@s.dcsdk12.org>
2020-02-11 09:18:45 -0700

    finish 4

--- src/IntArrayWorker.java
+++ src/IntArrayWorker.java
@@ -84,8 +84,7 @@ public class IntArrayWorker {
 			}
 		}
 	}
-	public int getCount(int checkNum)
-	{
+	public int getCount(int checkNum){
 	  int count = 0;
 	  for (int row = 0; row < matrix.length; row++)
 	  {
@@ -97,11 +96,9 @@ public class IntArrayWorker {
 	  return count;
 	}
 
-	public int getLargest()
-  {
+	public int getLargest() {
     int largest = 0;
-    for (int row = 0; row < matrix.length; row++)
-    {
+    for (int row = 0; row < matrix.length; row++){
       for (int col = 0; col < matrix[0].length; col++)
       {
         if (matrix[row][col] > largest) largest = matrix[row][col];
@@ -110,8 +107,7 @@ public class IntArrayWorker {
     return largest;
   }
 
-  public int getColTotal(int col)
-  {
+  public int getColTotal(int col){
     int total = 0;
     for (int row = 0; row < matrix.length; row++)
     {
